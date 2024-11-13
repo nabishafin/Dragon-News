994fed43850fe3adf9f9bcac2b65fee733fb9604
nabishafin <shafin21215@gmail.com>
2024-11-13 23:44:36 +0600

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,22 @@
+
+const Home = () => {
+
+
+    return (
+        <div>
+            <header>
+
+            </header>
+            <nav>
+
+            </nav>
+
+            <main>
+
+            </main>
+
+        </div>
+    );
+};
+
+export default Home;
